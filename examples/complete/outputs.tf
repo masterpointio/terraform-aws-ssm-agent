4e390ff4ec6dbe285dfb60f5d08dfb6dbfad0c79
Matt Gowie <matt@masterpoint.io>
2020-04-09 12:45:48 -0600

    Updates example outputs

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,15 @@
-output "ssm_agent_instance_id" {
-  value = module.ssm_agent.ssm_agent_instance_id
+output "ssm_agent_security_group_id" {
+  value = module.ssm_agent.security_group_id
+}
+
+output "ssm_agent_launch_template_id" {
+  value = module.ssm_agent.launch_template_id
+}
+
+output "ssm_agent_autoscaling_group_id" {
+  value = module.ssm_agent.autoscaling_group_id
+}
+
+output "ssm_agent_role_id" {
+  value = module.ssm_agent.role_id
 }
