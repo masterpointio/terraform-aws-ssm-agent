9a24118e6c371b5b94721162a444bfd03fb70f02
Mitch Hulscher <mitch.hulscher@lib.io>
2025-05-15 17:07:08 +0200

    refactor: turn var.additional_security_group_rules into map of objects

--- variables.tf
+++ variables.tf
@@ -64,8 +64,19 @@ variable "additional_security_group_ids" {
 
 variable "additional_security_group_rules" {
   description = "Additional security group rules that will be attached to the primary security group"
-  type        = map(string)
-  default     = {}
+  type = map(object({
+    type      = string
+    from_port = number
+    to_port   = number
+    protocol  = string
+
+    description      = optional(string)
+    cidr_blocks      = optional(list(string))
+    ipv6_cidr_blocks = optional(list(string))
+    prefix_list_ids  = optional(list(string))
+    self             = optional(bool)
+  }))
+  default = {}
 }
 
 variable "monitoring_enabled" {
